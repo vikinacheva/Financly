ab4eb18c9ec27819586040a9da76ab98329c5eec
vikinacheva <vikinacheva04@gmail.com>
2023-04-22 20:49:29 +0300

    history doneee

--- views/history/history.py
+++ views/history/history.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.metrics import dp, sp
+from datetime import datetime
 from kivy.utils import rgba, QueryDict, get_random_color, get_color_from_hex
 from kivy.properties import NumericProperty, ListProperty
 from widgets.tiles import ListTile
@@ -29,47 +29,38 @@ class History(Screen):
     
     def show_transactions(self, incomes, expenses):
         app = App.get_running_app()
-        self.monthly_incomes = app.monthly_incomes
-        self.monthly_expenses = app.monthly_expenses
         self.monthly_incomes = incomes
         self.monthly_expenses = expenses
-        
+
+        transactions = []
+        for income in incomes:
+            transactions.append(income)
+        for expense in expenses:
+            transactions.append(expense)
+
+        transactions.sort(key=lambda t: datetime.strptime(str(t[5]), "%Y-%m-%d %H:%M:%S"), reverse=True)
+
         self.ids.gl_history.clear_widgets()
-        
-        total_incomes = 0
-        total_expenses = 0
-        for income in self.monthly_incomes:
-            total_incomes += income[4]
-        for expense in self.monthly_expenses:
-            total_expenses += expense[4]
-          
-        for t in expenses:
+
+        total_incomes = sum(income[4] for income in incomes)
+        total_expenses = sum(expense[4] for expense in expenses)
+
+        for t in transactions:
             tile = ListTile(category_name=str(t[6]))
             tile.tile_id = str(t[0])
             tile.title = str(t[3])
             tile.subtitle = str(t[5])
             tile.amount = str(t[4])
             tile.budget_snapshot = str(t[7])
-            tile.expense = True
+            if t[2]:
+                tile.expense = True
+            else:
+                tile.expense = False
             tile.icon = category_icons.get(str(t[6]), "icons/default.png")
             tile.icon_color = get_color_from_hex("f8f9fa")
             tile.data = t
-            
+
             self.ids.gl_history.add_widget(tile)
 
-        for t in incomes:
-            tile = ListTile(category_name=str(t[6]))
-            tile.tile_id = str(t[0])
-            tile.title = str(t[3])
-            tile.subtitle = str(t[5])
-            tile.amount = str(t[4])
-            tile.budget_snapshot = str(t[7])
-            tile.expense = False
-            tile.icon = category_icons.get(str(t[6]), "icons/default.png")
-            tile.icon_color = get_color_from_hex("f8f9fa")
-            tile.data = t
-    
-            self.ids.gl_history.add_widget(tile)   
-            
         self.ids.monthly_expenses.text = f"-{total_expenses:.2f} лв."
-        self.ids.monthly_savings.text = f"{total_incomes - total_expenses:.2f} лв."    
+        self.ids.monthly_savings.text = f"{total_incomes - total_expenses:.2f} лв."
