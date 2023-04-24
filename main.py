1616a82d67926364ccaf40c0efbec4214cd58fcf
vikinacheva <vikinacheva04@gmail.com>
2023-04-24 13:24:53 +0300

    final

--- main.py
+++ main.py
@@ -71,12 +71,12 @@ class Financly(MDApp):
     def build(self):
         self.db = Database()
         screen_manager = ScreenManager()
-        screen_manager.add_widget(main.Main(name = "main"))
         screen_manager.add_widget(start.Start(name = "start"))
         screen_manager.add_widget(login.Login(name = "login"))
         screen_manager.add_widget(register.Register(name = "register"))
         screen_manager.add_widget(setup.Setup(name = "setup"))
         screen_manager.add_widget(welcome.Welcome(name = "welcome"))
+        screen_manager.add_widget(main.Main(name = "main"))
         
         return screen_manager   
             
